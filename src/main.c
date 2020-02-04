dd3511e70d4dc393b50babfe94023481dbb53123
Stephan Kleinert <stephan@ubisonic.de>
2020-02-04 12:04:32 +0100

    generating rooms

--- src/main.c
+++ src/main.c
@@ -5,9 +5,10 @@
 #include <cbm.h>
 #include <unistd.h>
 
-const int xDungeonSize = 80;
-const int yDungeonSize = 60;
-const int minFrameSize = 3;
+const int xDungeonSize = 40;
+const int yDungeonSize = 25;
+const int dungeonMinFrameSize = 4;
+const int minDungeonRoomCount = 0x0d;
 
 typedef unsigned char byte;
 
@@ -17,26 +18,41 @@ typedef enum
     dir_vertical
 } direction;
 
+typedef struct _room
+{
+    byte x0, y0, x1, y1;
+} room;
+
 typedef struct _frame
 {
     byte x0, y0, x1, y1;
     struct _frame *subframe1;
     struct _frame *subframe2;
+    room *frameRoom;
 } frame;
 
 void dumpFrame(frame *aFrame)
 {
     byte i;
-    revers(1);
-    for (i = aFrame->x0; i <= aFrame->x1; ++i)
+    room *theRoom;
+
+    theRoom = aFrame->frameRoom;
+
+    if (theRoom == NULL)
     {
-        cputcxy(i, aFrame->y0, ' ');
-        cputcxy(i, aFrame->y1, ' ');
+        return;
     }
-    for (i = aFrame->y0; i <= aFrame->y1; ++i)
+
+    revers(1);
+    for (i = theRoom->x0; i <= theRoom->x1; ++i)
     {
-        cputcxy(aFrame->x0, i, ' ');
-        cputcxy(aFrame->x1, i, ' ');
+        cputcxy(i, theRoom->y0, '*');
+        cputcxy(i, theRoom->y1, '*');
+    }
+    for (i = theRoom->y0; i <= theRoom->y1; ++i)
+    {
+        cputcxy(theRoom->x0, i, '*');
+        cputcxy(theRoom->x1, i, '*');
     }
     revers(0);
 }
@@ -65,7 +81,11 @@ frame *newFrame(byte x0, byte y0, byte x1, byte y1)
     aFrame->y1 = y1;
     aFrame->subframe1 = NULL;
     aFrame->subframe2 = NULL;
+    aFrame->frameRoom = NULL;
     // printf("new frame %d,%d,%d,%d at %x\n", x0, y0, x1, y1, aFrame);
+
+    gotoxy(0, 0);
+    cputhex16((int)aFrame);
     return aFrame;
 }
 
@@ -74,38 +94,37 @@ byte isLeaf(frame *aFrame)
     return (aFrame->subframe1 == NULL && aFrame->subframe2 == NULL);
 }
 
-void splitFrame(frame *aFrame)
+void splitFrame(frame *aFrame, byte minFrameSize)
 {
     direction splitDir;
     byte splitPoint;
     byte frameWidth;
     byte frameHeight;
-    int variance;
+    int frameSizeThreshhold;
     frame *subframe1 = NULL;
     frame *subframe2 = NULL;
 
     if (!aFrame)
+    {
         return;
+    }
 
-    variance = minFrameSize/2;
+    frameSizeThreshhold = (minFrameSize * 35) / 10;
 
     frameWidth = aFrame->x1 - aFrame->x0;
     frameHeight = aFrame->y1 - aFrame->y0;
 
     splitDir = (rand() & 1) ? dir_horizontal : dir_vertical;
 
-    if ((splitDir == dir_horizontal) && frameWidth >= (3 * minFrameSize))
+    if ((splitDir == dir_horizontal) && frameWidth >= frameSizeThreshhold)
     {
-        splitPoint = aFrame->x0 + frameWidth / 2;
-        splitPoint += (-variance + rand() % ((variance*2)+1));
-
+        splitPoint = aFrame->x0 + minFrameSize + (rand() % (frameWidth - (minFrameSize * 2)));
         subframe1 = newFrame(aFrame->x0, aFrame->y0, splitPoint, aFrame->y1);
         subframe2 = newFrame(splitPoint, aFrame->y0, aFrame->x1, aFrame->y1);
     }
-    else if ((splitDir == dir_vertical) && frameHeight >= (3 * minFrameSize))
+    else if ((splitDir == dir_vertical) && frameHeight >= frameSizeThreshhold)
     {
-        splitPoint = aFrame->y0 + frameHeight / 2;
-        splitPoint += (-variance + rand() % ((variance*2)+1));
+        splitPoint = aFrame->y0 + minFrameSize + (rand() % (frameHeight - (minFrameSize * 2)));
 
         subframe1 = newFrame(aFrame->x0, aFrame->y0, aFrame->x1, splitPoint);
         subframe2 = newFrame(aFrame->x0, splitPoint, aFrame->x1, aFrame->y1);
@@ -113,16 +132,8 @@ void splitFrame(frame *aFrame)
 
     aFrame->subframe1 = subframe1;
     aFrame->subframe2 = subframe2;
-    splitFrame(subframe1);
-    splitFrame(subframe2);
-}
-
-frame *createFrames(void)
-{
-    frame *startFrame;
-    startFrame = newFrame(0, 0, xDungeonSize - 1, yDungeonSize - 1);
-    splitFrame(startFrame);
-    return startFrame;
+    splitFrame(subframe1, minFrameSize);
+    splitFrame(subframe2, minFrameSize);
 }
 
 void deallocFrames(frame *aFrame)
@@ -133,21 +144,91 @@ void deallocFrames(frame *aFrame)
     }
     deallocFrames(aFrame->subframe1);
     deallocFrames(aFrame->subframe2);
+    if (aFrame->frameRoom)
+    {
+        free(aFrame->frameRoom);
+    }
     free(aFrame);
+}
+
+int _countLeafFrames(frame *startFrame, int currentCount)
+{
+
+    if (isLeaf(startFrame))
+    {
+        return currentCount + 1;
+    }
+    else
+    {
+        return _countLeafFrames(startFrame->subframe1, currentCount) +
+               _countLeafFrames(startFrame->subframe2, currentCount);
+    }
+}
+
+int countLeafFrames(frame *startFrame)
+{
+    return _countLeafFrames(startFrame, 0);
+}
+
+frame *createFrames(byte width, byte height, byte minFrameCount)
+{
+    frame *startFrame = NULL;
+    do
+    {
+        deallocFrames(startFrame);
+        startFrame = newFrame(0, 0, width - 1, height - 1);
+        splitFrame(startFrame, dungeonMinFrameSize);
+    } while (countLeafFrames(startFrame) < minFrameCount);
+    return startFrame;
+}
+
+void createRoomForFrame(frame *aFrame)
+{
+    room *newRoom;
+    int frameWidth;
+    int frameHeight;
+
+    frameWidth = aFrame->x1 - aFrame->x0;
+    frameHeight = aFrame->y1 - aFrame->y0;
+
+    newRoom = (room *)malloc(sizeof(room));
+    newRoom->x0 = aFrame->x0 + (rand() % ((frameWidth / 2) - 1));
+    newRoom->y0 = aFrame->y0 + (rand() % ((frameHeight / 2) - 1));
+    newRoom->x1 = aFrame->x1 - (rand() % ((frameWidth / 2) - 1));
+    newRoom->y1 = aFrame->y1 - (rand() % ((frameHeight / 2) - 1));
+    aFrame->frameRoom = newRoom;
+
+}
+
+void createRooms(frame *startFrame)
+{
+    if (isLeaf(startFrame))
+    {
+        createRoomForFrame(startFrame);
+    }
+    else
+    {
+        createRooms(startFrame->subframe1);
+        createRooms(startFrame->subframe2);
+    }
 }
 
 void main()
 {
     frame *aFrame = NULL;
-    textcolor(COLOR_GREEN);
+    textcolor(0x55);
     bgcolor(COLOR_BLACK);
     bordercolor(COLOR_BLACK);
+    clrscr();
     do
     {
         deallocFrames(aFrame);
+        aFrame = createFrames(xDungeonSize, yDungeonSize, minDungeonRoomCount);
+        createRooms(aFrame);
         clrscr();
-        aFrame = createFrames();
         dumpAllFrames(aFrame);
+        gotoxy(6, 0);
+        cputhex8(countLeafFrames(aFrame));
         sleep(1);
     } while (1);
 }
